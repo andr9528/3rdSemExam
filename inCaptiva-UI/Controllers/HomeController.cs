af8954d39765fdc4763b487057da292a9d1ef20a
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2017-11-14 13:42:48 +0100

    Kan hente data fra wcf og skrive det til cshtml 'en

--- inCaptiva-UI/Controllers/HomeController.cs
+++ inCaptiva-UI/Controllers/HomeController.cs
@@ -1,37 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using inCaptiva_UI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace inCaptiva_UI.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace inCaptiva_UI.Controllers {
+    public class HomeController : Controller {
+
+        public async Task<ActionResult> Index() {
+            var service = new InCaptivaService.InCaptivaServiceClient();
+            await service.NewWorkerAsync("Bob");
+            await service.NewWorkerAsync("Hans");
+            await service.NewWorkerAsync("Ryt");
+            await service.NewWorkerAsync("Knud");
+            List<InCaptivaService.Worker> data = await service.GetWorkersAsync();
+            return View(data);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId=Activity.Current?.Id??HttpContext.TraceIdentifier });
         }
     }
 }
